2edac8a0ce974eb79710a25493828e6fa3152b54
jslacasa <jsl@protonmail.com>
2026-02-08 01:00:58 +0200

    Quartz sync: Feb 8, 2026, 1:00 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -34,7 +34,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",//"#28503D",
-          secondary: "#4D705F",//"#284b63",
+          secondary: "#59816E",//"#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",

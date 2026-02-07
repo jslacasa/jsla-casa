7d6e4b5827c0a9d8146ff6d7c25f678b8064210f
jslacasa <jsl@protonmail.com>
2026-02-08 00:49:03 +0200

    Quartz sync: Feb 8, 2026, 12:49 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#28503D",//"#2b2b2b",
+          dark: "#2b2b2b",//"#28503D",
           secondary: "#4D705F",//"#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",

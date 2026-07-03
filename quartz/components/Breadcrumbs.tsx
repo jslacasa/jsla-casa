c7de499f7c49f7e0c747dde3999c2fb0864a482c
jslacasa <jsl@protonmail.com>
2026-07-03 13:25:23 +0300

    Quartz sync: Jul 3, 2026, 1:25 PM

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -32,7 +32,7 @@ const defaultOptions: BreadcrumbOptions = {
   spacerSymbol: "❯",
   rootName: "Home",
   resolveFrontmatterTitle: true,
-  showCurrentPage: true,
+  showCurrentPage: false, // default to false to avoid redundancy with page title
 }
 
 function formatCrumb(displayName: string, baseSlug: FullSlug, currentSlug: SimpleSlug): CrumbData {
